17f11ccfcf5454e545b9738ef2596f7023014c86
mr-augustine <pushbuttonrobotics@gmail.com>
2016-05-23 00:35:07 -0500

    Separated mallet code

--- demo_robot/demo_robot.ino
+++ demo_robot/demo_robot.ino
@@ -1,19 +1,13 @@
 #include <DistanceSensor.h>
-#include <Servo.h>
 
 #include "gong_bot.h"
 
-#define MALLET_PIN 7
 #define PING_LEFT_TRIG_PIN 9
 #define PING_LEFT_ECHO_PIN 8
 #define PING_CENTER_TRIG_PIN 12
 #define PING_CENTER_ECHO_PIN 13
 #define PING_RIGHT_TRIG_PIN 3
 #define PING_RIGHT_ECHO_PIN 2
-
-#define MALLET_EXTEND_POS 70
-#define MALLET_SNAP_POS 100
-#define MALLET_RETRACT_POS 160
 
 Servo mallet;
 
@@ -31,13 +25,13 @@ void setup() {
   pinMode(MOTOR_RIGHT_PIN_0, OUTPUT);
   pinMode(MOTOR_RIGHT_PIN_1, OUTPUT);
   
-  //mallet.attach(MALLET_PIN);
+  mallet_init(&mallet);
     
   Serial.begin(9600);
 
   // Wait a little while, then start driving forward
   delay(2000);
-  motor_drive_fwd(DRIVE_SLOW);
+  //motor_drive_fwd(DRIVE_SLOW);
   
   while(1) {}
 }
@@ -48,7 +42,6 @@ void loop() {
 
   //motor_drive_fwd(DRIVE_SLOW);
   //motor_drive_rev(DRIVE_SLOW);
-  //mallet_swing();
 
   left_ping_cm = left_ping.getDistance(true);
   right_ping_cm = right_ping.getDistance(true);
@@ -74,31 +67,4 @@ void loop() {
   //        retract the mallet, wait, reverse and turn as if an edge was detected on both sensors
   // 3.2 - Else keep moving forward (continue)
 }
-
-//////////////////////////////////////////////////////////////////////////
-// MALLET FUNCTIONS
-void mallet_swing(void) {
-  mallet.write(MALLET_EXTEND_POS);
-  delay(500);
-  mallet.write(MALLET_SNAP_POS);
-
-  /*mallet.write(MALLET_RETRACT_POS);
-  delay(2000);
-
-  mallet_swing();
-
-  delay(2000);
-  mallet.write(MALLET_RETRACT_POS);*/
-}
-
-void mallet_retract(void) {
-  mallet.write(MALLET_RETRACT_POS);
-  delay(1000);
-}
-
-//////////////////////////////////////////////////////////////////////////
-// MANEUVER FUNCTIONS
-void evade_edge(int danger_side) {
-}
-
 
