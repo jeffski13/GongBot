ef20b2205ef39f70b522fb920ddfe81ab27d69f1
mr-augustine <pushbuttonrobotics@gmail.com>
2016-05-22 20:05:05 -0500

    Changed pin vals after component reorg; psuedocode

--- demo_robot/demo_robot.ino
+++ demo_robot/demo_robot.ino
@@ -1,17 +1,18 @@
 #include <DistanceSensor.h>
 #include <Servo.h>
+#include "motor.h"
 
-#define MOTOR_LEFT_PIN_0 10
-#define MOTOR_LEFT_PIN_1 11
-#define MOTOR_RIGHT_PIN_0 5
-#define MOTOR_RIGHT_PIN_1 6
+#define MOTOR_LEFT_PIN_0 5
+#define MOTOR_LEFT_PIN_1 6
+#define MOTOR_RIGHT_PIN_0 11
+#define MOTOR_RIGHT_PIN_1 10
 #define MALLET_PIN 7
-#define PING_LEFT_TX_PIN 9
-#define PING_LEFT_RX_PIN 8
-#define PING_CENTER_TX_PIN 12
-#define PING_CENTER_RX_PIN 13
-#define PING_RIGHT_TX_PIN 3
-#define PING_RIGHT_RX_PIN 2
+#define PING_LEFT_TRIG_PIN 9
+#define PING_LEFT_ECHO_PIN 8
+#define PING_CENTER_TRIG_PIN 12
+#define PING_CENTER_ECHO_PIN 13
+#define PING_RIGHT_TRIG_PIN 3
+#define PING_RIGHT_ECHO_PIN 2
 
 #define DRIVE_SLOW 100
 #define DRIVE_MED  180
@@ -26,24 +27,25 @@ Servo mallet;
 
 long left_ping_cm;
 long right_ping_cm;
-long front_ping_cm;
+long center_ping_cm;
 
 void setup() {
   pinMode(MOTOR_LEFT_PIN_0, OUTPUT);
   pinMode(MOTOR_LEFT_PIN_1, OUTPUT);
   pinMode(MOTOR_RIGHT_PIN_0, OUTPUT);
   pinMode(MOTOR_RIGHT_PIN_1, OUTPUT);
-  
-  mallet.attach(MALLET_PIN);
-  mallet.write(MALLET_RETRACT_POS);
-  delay(2000);
 
-  mallet_swing();
-
-  delay(2000);
-  mallet.write(MALLET_RETRACT_POS);
+  DistanceSensor left_ping(PING_LEFT_TRIG_PIN, PING_LEFT_ECHO_PIN);
+  DistanceSensor right_ping(PING_RIGHT_TRIG_PIN, PING_RIGHT_ECHO_PIN);
+  DistanceSensor center_ping(PING_CENTER_TRIG_PIN, PING_CENTER_ECHO_PIN);
   
+  //mallet.attach(MALLET_PIN);
+    
   Serial.begin(9600);
+
+  // Wait a little while, then start driving forward
+  delay(2000);
+  motor_drive_fwd(DRIVE_SLOW);
 }
 
 void loop() {
@@ -51,8 +53,22 @@ void loop() {
   Serial.println(DRIVE_SLOW, DEC);
 
   //motor_drive_fwd(DRIVE_SLOW);
-  //motor_drive_rev(DRIVE_SLOW);
+  motor_drive_rev(DRIVE_SLOW);
   //mallet_swing();
+
+  return;
+
+  // 1 - Get the latest ping sensor readings
+
+  // 2 - Check if an edge was detected
+  // 2.1 - If edge on the right, then reverse, turn left, then continue
+  // 2.2 - Else if edge on the left, then reverse, turn right, then continue
+  // 2.3 - Else (if both) then reverse, turn either right or left, then continue
+
+  // 3 - Check if an obstacle was detected
+  // 3.1 - If and obstacle is withing striking distance, then: stop, swing the mallet,
+  //        retract the mallet, wait, reverse and turn as if an edge was detected on both sensors
+  // 3.2 - Else keep moving forward (continue)
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -79,21 +95,30 @@ void motor_turn_left(int speed) {
 void motor_turn_right(int speed) {
 }
 
-void motor_rev_left(int speed) {
-}
-
-void motor_rev_right(int speed) {
-}
-
 //////////////////////////////////////////////////////////////////////////
 // MALLET FUNCTIONS
 void mallet_swing(void) {
   mallet.write(MALLET_EXTEND_POS);
   delay(500);
   mallet.write(MALLET_SNAP_POS);
+
+  /*mallet.write(MALLET_RETRACT_POS);
+  delay(2000);
+
+  mallet_swing();
+
+  delay(2000);
+  mallet.write(MALLET_RETRACT_POS);*/
 }
 
 void mallet_retract(void) {
   mallet.write(MALLET_RETRACT_POS);
+  delay(1000);
 }
+
+//////////////////////////////////////////////////////////////////////////
+// MANEUVER FUNCTIONS
+void evade_edge(int danger_side) {
+}
+
 
